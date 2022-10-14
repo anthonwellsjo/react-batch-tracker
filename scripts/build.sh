d460de3447340913b6aafa3c0ef0449ce720b064
AnthonWellsjo <anthonwellsjo@gmail.com>
2022-10-14 20:15:30 +0200

    Build script is not cleaning dist folder

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
-echo "Deleting old package json in dist folder..."
-rm dist/package.json
+echo "✅ Done"
+echo "Transpiling transcript"
+npx tsc 
 echo "✅ Done"
 echo "Generating new package json to dist folder..."
 cp package.json dist/package.json
