f586d4a412e1e089e96cb9d54e651668a7c695ac
AnthonWellsjo <anthonwellsjo@gmail.com>
2022-10-23 13:03:04 +0200

    First react context test

---
+++ __tests__/operations.test.tsx
@@ -0,0 +1,40 @@
+import {render, screen } from '@testing-library/react'
+import {BatchTrackerContext, BatchTrackerProvider, Tracker} from '../lib/index';
+import { useContext, useEffect, useState } from 'react';
+
+const testTracker = {
+  name: "test",
+  callbackTimeout: 1000,
+  callbackFunction: (data: any) => {console.log(data)},
+}
+
+
+test("Batch tracker successfully creates a new batch", () => {
+const Consumer: React.FC = () => {
+
+  console.log("running tests");
+  const { createTracker, getBatchTracker } = useContext(BatchTrackerContext);
+
+  const [ tracker, setTracker ] = useState<Tracker<unknown & {id: string}> | undefined>(undefined);
+
+  useEffect(()=>{
+      createTracker(testTracker.name, testTracker.callbackTimeout, testTracker.callbackFunction);
+  }, []);
+
+
+  useEffect(()=>{
+      setTracker(getBatchTracker(testTracker.name));
+
+  }, []);
+  
+  return <p id="tracker-name">{tracker.name}</p>
+}
+
+  render(
+    <BatchTrackerProvider>
+      <Consumer/>
+    </BatchTrackerProvider>,
+  )
+  expect(screen.getByTestId('tracker-name').textContent).toBe(testTracker.name)
+})
+
