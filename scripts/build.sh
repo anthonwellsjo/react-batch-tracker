7db2d3d00f639d723d4421f22925a51e20a4b07f
AnthonWellsjo <anthonwellsjo@gmail.com>
2022-11-11 22:33:18 +0100

    updating build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,8 @@ echo "✅ Done"
 echo "Transpiling transcript"
 npx tsc 
 echo "✅ Done"
-echo "Generating new package json to dist folder..."
+echo "Copying files for publishing to the dist folder..."
 cp package.json dist/package.json
+cp README.md dist/README.md
+cp LICENSE dist/LICENSE
 echo "✅ Done"
